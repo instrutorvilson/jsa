e4c24f05952887829d34a3e74cf4158de588eca9
Vilson Moro <vilson.moro@PROWAY.TREINA>
2023-10-28 11:18:18 -0300

    consumir api viacep

---
+++ variaveis.js
@@ -0,0 +1,24 @@
+const outraConst = "ola const"
+var x = 10
+console.log(x)
+
+x = "ola mundo"
+console.log(x)
+
+if(2==2){
+   var  y = 100
+    console.log(y)
+}
+console.log(y)
+ 
+const z ="Minhabase"
+console.log(z)
+/**it not possible */
+//z = "minhaoutrabase"
+if(2==2){
+    const w = "123"
+    console.log(w)
+    console.log(outraConst)
+}
+//console.log(w)
+console.log(outraConst)
