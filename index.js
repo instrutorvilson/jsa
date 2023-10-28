0736d2e42a3e4a12182e8c80087a04053aea203d
Vilson Moro <vilson.moro@PROWAY.TREINA>
2023-10-28 11:14:50 -0300

    Conceitos básicos

---
+++ index.js
@@ -0,0 +1,17 @@
+console.log("ola mundo")
+console.log("hello world")
+
+/**Declaração da função */
+function somar(){
+   console.log("Somando")
+}
+
+/**Invocar a função */
+somar()
+
+function subtrair(n1,n2){
+    return n1 - n2
+}
+
+var x = subtrair(20, 10)
+console.log(x)
